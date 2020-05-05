599d893e23256621bcda6b988e24973b5ca52e8a
José <josejustinno@gmail.com>
2020-05-05 14:06:58 -0300

    Criação da Api de categorias

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using shop.Data;
+using shop.Models;
+
+namespace shop.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Get(
+            [FromServices]DataContext context
+        )
+        {
+            var categories = await context.Categories.AsNoTracking().ToListAsync();
+
+            if (categories == null)
+                return NotFound(new { message = "Nenhuma categoria foi encontrada" });
+
+            return Ok(categories);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> GetById(
+            int id,
+            [FromServices]DataContext context
+        )
+        {
+            var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound(new { message = "Categoria não encontrada" });
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> Post(
+            [FromBody]Category model,
+            [FromServices] DataContext context
+        )
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+
+                return Ok(model);
+            }
+            catch
+            {
+                return BadRequest(new { message = "Não foi possível criar a categoria" });
+            }
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Category>>> Put(
+            int id,
+            [FromBody]Category model,
+            [FromServices]DataContext context)
+        {
+            // Verifica se o ID informado é o mesmo do modelo
+            if (id != model.Id)
+                return NotFound(new { Message = "Categoria não encontrada" });
+
+            // Verifica se os dados são válidos
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Entry<Category>(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest(new { message = "Este registro já foi atualizado" });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível atualizar a categoria" });
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Category>>> Delete(
+            int id,
+            [FromServices]DataContext context
+        )
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+                return NotFound(new { message = "Categoria não encontrada" });
+
+            try
+            {
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+                return Ok(category);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível remover a categoria" });
+            }
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using shop.Models;
+
+namespace shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        // public DbSet<User> Users { get; set; }
+    }
+}
