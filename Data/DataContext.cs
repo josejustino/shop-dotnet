6bdd8a57b95d40eba067a6f8215f49ea398cc646
José <josejustinno@gmail.com>
2020-05-07 21:47:18 -0300

    adicionar dbset de usuários

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -12,6 +12,6 @@ namespace shop.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        // public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
